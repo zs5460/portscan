dfcb43be76dccfee411888a2027eb8590fba8060
zs5460 <zs5460@gmail.com>
2019-06-05 10:05:47 +0800

    fix:limiter does not take effect

--- main.go
+++ main.go
@@ -16,21 +16,18 @@ var (
 )
 
 func isOpen(addr string) bool {
-	conn, err := net.DialTimeout("tcp", addr, time.Second*2)
+	_, err := net.DialTimeout("tcp", addr, time.Second*2)
 	if err != nil {
-		//log.Println(err)
 		return false
 	}
-	conn.Close()
 	return true
 }
 
 func scan(ip string, ports []string) {
 	limit := make(chan struct{}, 1000)
 	for _, p := range ports {
+		limit <- struct{}{}
 		go func(addr string) {
-			limit <- struct{}{}
-			//fmt.Println("scan ", addr)
 			wg.Add(1)
 			if isOpen(addr) {
 				fmt.Printf("%s is open\n", addr)
@@ -42,7 +39,7 @@ func scan(ip string, ports []string) {
 }
 
 func QuickScan(ip string) {
-	commonPorts := "21,22,23,25,53,80,110,135,137,138,139,443,1433,1434,1521,3306,3389,5000,5432,5632,6379,8000,8080,8081,9090,10051,11211,27017"
+	commonPorts := "21,22,23,25,53,80,110,135,137,138,139,443,1433,1434,1521,3306,3389,5000,5432,5632,6379,8000,8080,8081,8443,9090,10051,11211,27017"
 	ports := strings.Split(commonPorts, ",")
 	scan(ip, ports)
 }
@@ -69,21 +66,21 @@ func main() {
 `
 
 	ip := ""
-	quickMode := false
+	fullMode := false
 
 	fmt.Println(banner)
 
 	flag.StringVar(&ip, "ip", "localhost", "IP address to scan")
-	flag.BoolVar(&quickMode, "q", false, "Quick mode scans only common ports")
+	flag.BoolVar(&fullMode, "f", false, "Full scan mode scans all ports, the default is off, the default only scans common ports")
 	flag.Parse()
 
 	startTime := time.Now()
-	if quickMode {
-		fmt.Println("Start a quick scan...")
-		QuickScan(ip)
-	} else {
+	if fullMode {
 		fmt.Println("Start a full scan...")
 		FullScan(ip)
+	} else {
+		fmt.Println("Start a quick scan...")
+		QuickScan(ip)
 	}
 
 	wg.Wait()
