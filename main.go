7bd64a19352f3e237d12616548b8293380b13d9f
zs5460 <zs5460@gmail.com>
2019-07-25 20:45:19 +0800

    feat:add udp scan

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var (
 )
 
 func isOpen(addr string) bool {
-	conn, err := net.DialTimeout("tcp", addr, time.Second*2)
+	conn, err := net.DialTimeout("tcp", addr, time.Second*1)
 	if err != nil {
 		// if strings.Index(err.Error(), "timeout") < 0 {
 		// 	log.Println(err)
@@ -32,7 +32,42 @@ func isOpen(addr string) bool {
 	return true
 }
 
-func scan(ip string, ports []string) {
+func isOpenUDP(addr string) bool {
+	udpaddr, err := net.ResolveUDPAddr("udp4", addr)
+	if err != nil {
+		return false
+	}
+	conn, err := net.DialUDP("udp", nil, udpaddr)
+	defer conn.Close()
+	if err != nil {
+		return false
+	}
+	conn.SetDeadline(time.Now().Add(time.Duration(time.Second * 1)))
+	var data []byte
+	switch udpaddr.Port {
+	case 53:
+		data = []byte("\x24\x1a\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x03\x77\x77\x77\x06\x67\x6f\x6f\x67\x6c\x65\x03\x63\x6f\x6d\x00\x00\x01\x00\x01")
+	case 123:
+		data = []byte("\xe3\x00\x04\xfa\x00\x01\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xc5\x4f\x23\x4b\x71\xb1\x52\xf3")
+	case 161:
+		data = []byte("\x30\x2c\x02\x01\x00\x04\x07\x70\x75\x62\x6c\x69\x63\xA0\x1E\x02\x01\x01\x02\x01\x00\x02\x01\x00\x30\x13\x30\x11\x06\x0D\x2B\x06\x01\x04\x01\x94\x78\x01\x02\x07\x03\x02\x00\x05\x00")
+	default:
+		data = []byte("\xff\xff\x70\x69\x65\x73\x63\x61\x6e\x6e\x65\x72\x20\x2d\x20\x40\x5f\x78\x39\x30\x5f\x5f")
+	}
+	_, err = conn.Write(data)
+	if err != nil {
+		return false
+	}
+
+	buf := make([]byte, 256)
+	_, err = conn.Read(buf)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func scan(proto string, ip string, ports []string) {
 	var wg sync.WaitGroup
 	limiter := make(chan struct{}, maxThread)
 	result := make(chan string, 64)
@@ -47,10 +82,16 @@ func scan(ip string, ports []string) {
 		limiter <- struct{}{}
 		go func(addr string) {
 			defer wg.Done()
-
-			if isOpen(addr) {
-				result <- fmt.Sprintf("%s is open", addr)
+			if proto == "udp" {
+				if isOpenUDP(addr) {
+					result <- fmt.Sprintf("[UDP] %22s is open", addr)
+				}
+			} else {
+				if isOpen(addr) {
+					result <- fmt.Sprintf("[TCP] %22s is open", addr)
+				}
 			}
+
 			<-limiter
 		}(ip + ":" + p)
 
@@ -68,19 +109,20 @@ func FullScan(ip string) {
 		tmp[i] = strconv.Itoa(i + 1)
 	}
 	ports := tmp[:]
-	scan(ip, ports)
+	scan("tcp", ip, ports)
+	scan("udp", ip, ports)
 }
 
 // QuickScan scan only common ports.
 func QuickScan(ip string) {
 	commonPorts := "21,22,23,25,53,80,110,135,137,138,139,443,1433,1434,1521,3306,3389,5000,5432,5632,6379,8000,8080,8081,8443,9090,10051,11211,27017"
 	ports := strings.Split(commonPorts, ",")
-	scan(ip, ports)
+	scan("tcp", ip, ports)
 }
 
 // SpecifiedScan scan specified port.
 func SpecifiedScan(ip, port string) {
-	scan(ip, strings.Split(port, ","))
+	scan("tcp", ip, strings.Split(port, ","))
 }
 
 // ScanIPS scan multiple IPs.
@@ -114,6 +156,7 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	banner := `
+                      __                      	
     ____  ____  _____/ /_______________ _____ 
    / __ \/ __ \/ ___/ __/ ___/ ___/ __ \/ __ \
   / /_/ / /_/ / /  / /_(__  ) /__/ /_/ / / / /
